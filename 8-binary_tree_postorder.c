6a71fa526907545dac3980c2df827928488b0a53
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-11-15 21:23:30 +0100

    task: Postorder

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - Goes through a tree using postorder
+ * @tree: the tree
+ * @func: the func
+ * Return: void
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	if (tree->left)
+		binary_tree_postorder(tree->left, func);
+	if (tree->right)
+		binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
